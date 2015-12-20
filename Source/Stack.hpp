0d0407b4d724edae95cea701e6485038d544a3ab
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-20 00:59:57 -0500

    Templatizes the Stack class

--- Source/Stack.hpp
+++ Source/Stack.hpp
@@ -3,40 +3,90 @@
 
 #include <iostream>
 
-#include "Card.hpp"
-
+template <class T> 
 class Stack {
 public:
-	Stack ( );
+	Stack ( ) : capacity (13), size (0), stack ( stack = new T*[capacity]) { }
 
-	Stack (int c);
+	Stack (int c) : capacity (c), size (0), stack (nullptr) {
+		if (c < 0) { capacity = 0; }
+		stack = new T*[capacity];
+	}
 
-	Stack (const Stack& rhs);
+	Stack (const Stack& rhs) : capacity (rhs.capacity), stack (nullptr) {
+		stack = new T*[capacity];
+		for (size = 0; size < rhs.size; ++size) {
+			stack[size] = new T (*rhs.stack[size]);
+		}
+	}
 
-	~Stack ( );
+	~Stack ( ) {
+		for (int i = 0; i < size; ++i) {
+			delete stack[i];
+		}
+		delete[] stack;
+	}
 
-	int getSize ( ) const;
+	int getSize ( ) const {
+		return size;
+	}
 
-	int getTotalCapacity ( ) const;
+	int getTotalCapacity ( ) const {
+		return capacity;
+	}
 
-	bool isEmpty ( ) const;
+	bool isEmpty ( ) const {
+		return size == 0;
+	}
 
-	bool isFull ( ) const;
+	bool isFull ( ) const {
+		return size == capacity;
+	}
 
-	Card* peek ( ) const;
+	T* peek ( ) const {
+		return stack[size - 1];
+	}
 
-	Card* pop ( );
+	T* pop ( ) {
+		if (! isEmpty ( )) {
+			return stack[--size];
+		}
+		return nullptr;
+	}
 
-	void push (Card* c);
+	void push (T* c) {
+		if(!isFull()) {
+			stack[size++] = c;
+		}
+	}
 
-	Stack& operator=(const Stack& rhs);
+	Stack& operator=(const Stack& rhs) {
+		if (this != &rhs) {
+			for (int i = 0; i < size; ++i) {
+				delete stack[i];
+			}
+			delete[] stack;
 
-	friend std::ostream& operator<<(std::ostream& output, const Stack& currentStack);
+			capacity = rhs.capacity;
+			stack = new T*[capacity];
+			for (size = 0; size < rhs.size; ++size) {
+				stack[size] = new T (*rhs.stack[size]);
+			}
+		}
+		return *this;
+	}
+
+	friend std::ostream& operator<<(std::ostream& output, const Stack& currentStack) {
+		for (int i = 0; i < currentStack.size; ++i) {
+			output << *currentStack.stack[i] << "\n";
+		}
+		return output;
+	}
 
 private:
 	int capacity;
 	int size;
-	Card** stack;
+	T** stack;
 };
 
 #endif
