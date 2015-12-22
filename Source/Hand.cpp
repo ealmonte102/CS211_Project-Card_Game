2bf32dac6f3e90811694f2ff65de1435c8d24f36
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-22 17:54:48 -0500

    Implements removeGroup and fixes a typo on line 20 causing a bug
    -	bug fix: == sign changed to an = sign.

--- Source/Hand.cpp
+++ Source/Hand.cpp
@@ -5,6 +5,28 @@
 
 Hand::Hand ( ) : LinkedList<Card*> ( ) {}
 
+void Hand::removeGroup(Node* node) {
+	if (node == nullptr) { return; }
+	Node* previous = head;
+	Node* current = head;
+	while(current != node) {
+		previous = current;
+		current = current->next;
+	}
+	Node* afterDelete = nullptr;
+	for (int i = 0; i < 4; i++) {
+		afterDelete = current->next;
+		delete current;
+		--size;
+		current = afterDelete;
+	}
+	if(previous != head) {
+		previous->next = afterDelete;
+	} else {
+		head = afterDelete;
+	}
+}
+
 LinkedList<Card*>::Node* Hand::findGroup( ) const {
 	if (head == nullptr) { return nullptr; }
 	Node* current = head;
@@ -17,7 +39,7 @@ LinkedList<Card*>::Node* Hand::findGroup( ) const {
 			if (groupCount == 4) { return startOfGroup; }
 		} else {
 			startOfGroup = current->next;
-			groupCount == 1;
+			groupCount = 1;
 		}
 		current = current->next;
 	}
