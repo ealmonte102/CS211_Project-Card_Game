46379add49a0d77f223d30cd5f42f336ecfd84b8
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-24 14:27:52 -0500

    Creates the Player class and declares various functions
    -	Constructor.
    -	getScore, getName, isHuman.
    -	askForCard, addCard, evaluateHand.

---
+++ Source/Player.cpp
@@ -0,0 +1,6 @@
+﻿// Player.cpp
+// Created by Evan Almonte
+//
+#include "Player.hpp"
+
+
---
+++ Source/Player.hpp
@@ -0,0 +1,26 @@
+﻿#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include "Card.hpp"
+#include "Hand.hpp"
+#include <string>
+
+class Card;
+class Player {
+public:
+	Player (std::string name, bool isCPU);
+	int getScore ( ) const;
+	std::string getName ( ) const;
+	bool isHuman ( ) const;
+	void addCard (Card* cardToAdd);
+	bool askForCard (Player& otherPlayer, Card::Suits aSuit, Card::Ranks aRank);
+	void evaluateHand ( );
+private:
+	std::string name;
+	bool isCPU;
+	Hand playerHand;
+	int score;
+};
+
+#endif //PLAYER_HPP
+
