097d17b52fc7b2551d7042cabf00488a14aaf66d
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-24 20:29:37 -0500

    Implements various member functions of the Player class
    -	2-Parameter Constructor(string, bool).
    -	getScore(), getName(), isHuman().
    -	addCard(), askForCard(), evaluateHand().

--- Source/Player.cpp
+++ Source/Player.cpp
@@ -3,4 +3,28 @@
 //
 #include "Player.hpp"
 
+using std::cout;
 
+Player::Player(std::string name, bool isCPU) : name(name), isCPU(isCPU), score(0) {}
+
+int Player::getScore ( ) const { return score; }
+
+std::string Player::getName ( ) const { return name; }
+
+bool Player::isHuman ( ) const { return !isCPU; }
+
+void Player::addCard (Card* cardToAdd) { playerHand.insert (cardToAdd); }
+
+bool Player::askForCard(Player& otherPlayer, Card::Suits aSuit, Card::Ranks aRank) {
+	Card cardToFind (aSuit, aRank);
+	if (playerHand.find (&cardToFind) == nullptr) { return false; };
+	Card* cardFound = otherPlayer.playerHand.find (&cardToFind);
+	if (cardFound == nullptr) { return false; }
+	playerHand.insert (cardFound);
+	otherPlayer.playerHand.remove (cardFound);
+	return true;
+}
+
+void Player::evaluateHand( ) {
+	score = playerHand.evaluate();
+}
