50448618135e813c692f35e41e0aa502bc7458ba
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-26 04:53:40 -0500

    Adds new class GoFishGame and declare/defines 2 constructors
    -	Default Constructor.
    -	1 parameter int Constructor.

---
+++ Source/GoFishGame.cpp
@@ -0,0 +1,16 @@
+﻿// GoFishGame.cpp
+// Created by Evan Almonte
+//
+#include "GoFishGame.hpp"
+
+const int GoFishGame::defaultNumOfPlayers = 2;
+
+GoFishGame::GoFishGame( ) : theDeck(Deck(52)), numOfPlayers(defaultNumOfPlayers),
+							thePlayers(new Player[defaultNumOfPlayers]) { }
+
+GoFishGame::GoFishGame(int numOfPlayers) : theDeck(Deck(52)), numOfPlayers(numOfPlayers) {
+	if(numOfPlayers < 2) {
+		this->numOfPlayers = defaultNumOfPlayers;
+	}
+	thePlayers = new Player[this->numOfPlayers];
+}
---
+++ Source/GoFishGame.hpp
@@ -0,0 +1,20 @@
+﻿#ifndef GOFISHGAME_HPP
+#define GOFISHGAME_HPP
+
+#include "Deck.hpp"
+#include "Player.hpp"
+
+class GoFishGame {
+public:
+	GoFishGame ( );
+	GoFishGame (int numOfPlayers);
+	void play ( );
+private:
+	Deck theDeck;
+	static const int defaultNumOfPlayers;
+	int numOfPlayers;
+	Player* thePlayers;
+};
+
+#endif //GOFISHGAME_HPP
+
