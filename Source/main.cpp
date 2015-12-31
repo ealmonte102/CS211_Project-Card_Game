da5eb9de3c16c514d3aa063c80444a952586dcf2
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-30 23:52:01 -0500

    Adds main to test GoFishGame

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,20 +2,13 @@
 // Created by Evan Almonte on 10/29/2015.
 //
 
-#include <iostream>
+#include "GoFishGame.hpp"
 #include <cstdlib>
 #include <ctime>
-#include "Deck.hpp"
-
-using std::cout;
 
 int main ( ) {
-	srand (static_cast<unsigned int>(time (NULL)));
-	Deck original;
-	cout << "---------Before Shuffle---------\n";
-	cout << original;
-	original.shuffle ( );
-	cout << "\n---------After Shuffle---------\n";
-	cout << original;
+	srand (static_cast<unsigned int>(time (nullptr)));
+	GoFishGame* aGame = new GoFishGame(2);
+	aGame->play ( );
 	return 0;
 }
